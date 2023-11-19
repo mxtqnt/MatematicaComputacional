503b652e929a7c4a29d5d0ef7140de6478b9ebd2
Mxtqnt <anacarolinass.cc@gmail.com>
2023-11-19 01:17:34 -0300

    Fix: Final

--- l5ex19.m
+++ l5ex19.m
@@ -2,22 +2,36 @@ function l5ex19()
   clc;
   x = [2, 3, 4, 5, 6, 7];
   y = [0.5, 0.3333, 0.25, 0.2, 0.1667, 0.1429];
-  
-  %x = [0, 1, 2, 3, 4, 5];
-  %y = [2.1, 7.7, 13.6, 27.2, 40.9, 61.1];
+  xdef = 5.5;
   
   ordem = input("Defina o grau do polinonimo: ");
-  fprintf("Ordem escolhida: %i \n", ordem)
   
-  coeficientes = elementos(ordem, x, y)
-  [r2, coeficiteDeterminacao] = determinacao(coeficientes, x, y)
+  coeficientes = elementos(ordem, x, y);
+  [r2, coeficiteDeterminacao] = determinacao(coeficientes, x, y);
   
-  plotrosa(ordem, x, y)
+  fprintf("Os pontos: ") 
+  for i = 1:numel(x)
+    fprintf("(%d, %d) ", x(i), y(i))    
+  endfor
+  
+  fprintf("\nResultaram nos coeficientes: \n")
+  disp (coeficientes)
+  fprintf("Gerando a função: \nf(x) = ")
+  fprintf("%d + ", coeficientes(1))
+  for i = 2:numel(coeficientes)
+    fprintf("%d.x^%d", coeficientes(i), (i-1))
+    if i <= (numel(coeficientes)-1)
+      fprintf(" + ")
+    endif
+  endfor
+  
+  fprintf("\nEstimando x para %d o polinomio resulta em: %d\nR2=%d, R=%d\n", xdef, funcao(coeficientes, xdef), r2, coeficiteDeterminacao);
+  plotrosa(ordem, x, y);
 endfunction
 
 function x = elementos(ordem, x, y)
   matrizA = zeros(ordem+1, ordem+2);
-  [~, n] = size(x)
+  [~, n] = size(x);
   for i = 1:(ordem+1)
     for j = 1:i
       k = i+j-2;
@@ -51,19 +65,19 @@ function [r2, coeficiteDeterminacao] = determinacao(coeficientes, x, y)
   coeficiteDeterminacao = sqrt(r2);
 endfunction
 
-function y = funcao(coeficientes, x)
+function y = funcao(coeficientes, xdef)
   y = 0;
   for i = numel(coeficientes):-1:1 % Decrementa os coeficientes
-    y = y + coeficientes(i) * x^(i - 1);
+    y = y + coeficientes(i) * xdef^(i - 1);
   endfor
 endfunction
 
 function plotrosa(ordem, x, y)
-  p = polyfit(x, y, ordem)
-  for i = 1:numel(x)
-    plot(x(i), y(i), 'marker', 'o', 'markersize', 10, 'markerfacecolor', [0.9922, 0.0000, 0.5490], 'DisplayName', 'Pontos')
-    hold on;
-  endfor
-  plot(x, polyval(p, x), 'linewidth', 4, 'color', [0.9922, 0.0000, 0.5490])
+  p = polyfit(x, y, ordem);
+  plot(x, y, 'marker', 'o', 'markersize', 10, 'markerfacecolor', [0.9922, 0.0000, 0.5490],'linewidth', 1, 'color', [1, 1, 1], 'DisplayName', 'Pontos');
+  hold on;
+  grid on;
+  plot(x, polyval(p, x), 'linewidth', 2, 'color', [0.9922, 0.0000, 0.5490]);
+  legend("Pontos", "Polinomio");
   hold off;
 endfunction
